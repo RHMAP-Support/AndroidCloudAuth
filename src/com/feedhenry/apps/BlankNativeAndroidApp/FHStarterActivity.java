8f22000f7c6a23e622c0577c32f45d4ff7d5be09
tomcat6 <tomcat6@tom1-core-app1>
2016-01-20 15:49:23 +0000

    Initialize app from template

---
+++ src/com/feedhenry/apps/BlankNativeAndroidApp/FHStarterActivity.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2015 Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feedhenry.apps.BlankNativeAndroidApp;
+
+import com.feedhenry.sdk.FH;
+import com.feedhenry.sdk.FHActCallback;
+import com.feedhenry.sdk.FHResponse;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FHStarterActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fhstarter);
+
+        //call FH.init to initialize FH service
+        FH.init(this, new FHActCallback() {
+
+            @Override
+            public void success(FHResponse resp) {
+                //NOTE: other FH methods can only be called after FH.init succeeds
+            }
+
+            @Override
+            public void fail(FHResponse arg0) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_fhstarter, menu);
+        return true;
+    }
+}
